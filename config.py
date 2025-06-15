b55e31a7cc33e485e8118730e3bea81aa242b8ff
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2025-06-15 15:46:25 -0300

    Data munging (#4)
    * atualizando periodo do estudo para bater com o perido oda politica publica
    
    * adicionando coluna que identifica se é PEI
    
    * fix para remover escolas tecnicas
    
    * filtrando para apenas ensino medio
    
    * data munging para modelagem finalizado
    
    * quantidades de anos ofertou integral

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 
 DATA_FOLDER= 'data'
-RANGE_ANOS_ESTUDO: list[int] = list(range(2012, 2025)) #2012 a 2024
+RANGE_ANOS_ESTUDO: list[int] = list(range(2011, 2019)) #2011 a 2018 - período da política pública estudada
